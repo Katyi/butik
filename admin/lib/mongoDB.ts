4931dd41f02b48dc5ad324811fa83a9f73fd9e1e
Alexandra Egorova <egorova.aleksandra@gmail.com>
2024-10-03 08:11:33 +0200

    fixed some bug after deployment

--- admin/lib/mongoDB.ts
+++ admin/lib/mongoDB.ts
@@ -12,7 +12,7 @@ export const connectToDB = async (): Promise<void> => {
 
   try {
     await mongoose.connect(process.env.MONGODB_URL || '', {
-      dbName: 'Borcella_Admin',
+      dbName: 'butik_admin',
     });
 
     isConnected = true;
--- client/lib/mongoDB.ts
+++ client/lib/mongoDB.ts
@@ -12,7 +12,7 @@ export const connectToDB = async (): Promise<void> => {
 
   try {
     await mongoose.connect(process.env.MONGODB_URL || '', {
-      dbName: 'Borcella_Store',
+      dbName: 'butik_store',
     });
 
     isConnected = true;
